9d54100ab46ec691d1d9ae4f1b45e8a389dc218a
Sergi Dote Teixidor <sergidt@gmail.com>
2020-08-18 15:00:10 +0200

    Cleaning code

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { Observable, combineLatest } from 'rxjs';
 import { Item, FILTERING_DATA, FilteringData } from '../definitions';
-import { withLatestFrom, startWith, map, distinctUntilChanged, tap } from 'rxjs/operators';
+import { startWith, map, distinctUntilChanged } from 'rxjs/operators';
 
 @Injectable()
 export class SearchService {
@@ -11,7 +11,7 @@ export class SearchService {
   filterByTerm(search$: Observable<string>): Observable<Array<Item>> {
     return combineLatest([
       this.filteringData.dataSource,
-      search$.pipe(tap(_ => console.log('tap', _)), startWith(''))
+      search$.pipe(startWith(''))
     ])
         .pipe(
             map(([items, term]: [Array<Item>, string]) => items.filter(_ => this.filteringData.filterFunction(_, term))),

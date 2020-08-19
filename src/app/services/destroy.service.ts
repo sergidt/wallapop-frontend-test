21bb0c2d544fd582c3babbf2c5a230782e9bbbae
Sergi Dote Teixidor <sergidt@gmail.com>
2020-08-19 19:54:55 +0200

    Added: Destroy Service

---
+++ src/app/services/destroy.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class DestroyService extends Observable<void> implements OnDestroy {
+  private readonly life$ = new Subject<void>();
+
+  constructor() {
+    super(subscriber => this.life$.subscribe(subscriber));
+  }
+
+  ngOnDestroy() {
+    this.life$.next();
+    this.life$.complete();
+  }
+}
